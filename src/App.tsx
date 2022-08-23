b8117f0fa2d6c55960c5b56673c460c6d28dcf2e
Raimundo <raimundoladrondeguevara@gmail.com>
2022-08-23 09:46:55 -0400

    tercer commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Rectangle from './components/ViewRectangle/Rectangle';
 import Post from './components/Post/Post';
+import PostContainer from './components/PostContainer/PostContainer';
 
 const App: React.FC = () => {
   return (
